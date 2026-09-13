d9e1e94ca3869509ce413b3d83da80585102d2ed
nadimul5555-netizen < GitHub- Email>
2026-09-13 13:23:43 +0600

    Commit-2:Add Navber

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { Suspense, useState } from 'react'
+import Nav from './component/Nav'
+import Banner from './component/Banner'
+import Technologies from './component/Technologies';
+import  type{ TechTypes } from './TechTypes';
+
+const TechDataFetch = async():Promise<TechTypes[]>=>{
+  const res = await fetch("/data.json");
+  const data = await res.json();
+  return data
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [TechPromis]=useState(()=>TechDataFetch())
+  
   return (
     <>
-    
-    
+  <Nav></Nav>
+  <Banner></Banner>
+  <Suspense fallback={<p>Loadding ....</p>}>
+
+  <Technologies TechPromis={TechPromis} ></Technologies>
+  </Suspense>
     </>
   )
 }
---
+++ src/component/Nav.tsx
@@ -0,0 +1,26 @@
+
+
+const Nav = () => {
+  return (
+    <div className="grid grid-cols-3  container mx-auto sticky h-25 ">
+        <div className="content-center mx-5 ">
+            <img className="h-14" src="/assets/logo-text.png" />
+        </div>
+        <div className="flex justify-center items-center">
+            <ul className="flex gap-7 text-[#475569]">
+                <li>Home</li>
+                <li>Technologies</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Connect</li>
+            </ul>
+        </div>
+        <div className="flex justify-end mx-5 items-center gap-5">
+            <h2>Sign In</h2>
+            <button className="btn btn-secondary rounded-3xl">Sign Up</button>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
